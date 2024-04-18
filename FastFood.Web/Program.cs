efddc61181ed204507cdfc5b4e3d2b1b0e2082ec
nimur40 <nimur40@gmail.com>
2024-04-18 15:24:45 +0600

    add dbContext

--- FastFood.Web/Program.cs
+++ FastFood.Web/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+//builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContextConnection")));
 
 var app = builder.Build();
 
